d62a5fc338a1ba048c050d276269df950dd32aa9
Ruan Pearce-Authers <ruanpa@outlook.com>
2020-03-13 12:17:04 +0000

    Initial commit

---
+++ R/overrides.R
@@ -0,0 +1,34 @@
+
+#' @export
+connect_snowflake <- function(user, password, host, warehouse, role, database = NULL, schema = NULL, bootstrap_session = TRUE) {
+	con <- DBI::dbConnect(
+		odbc::odbc(),
+		UID = user,
+		PWD = password,
+		Server = host,
+		Warehouse = warehouse,
+		Role = role,
+		Driver = "Snowflake",
+		Database = database,
+		Schema = schema
+	)
+
+	if (bootstrap_session) {
+		DBI::dbGetQuery(con, "alter session set quoted_identifiers_ignore_case = true")
+	}
+
+	con
+}
+
+#' @export
+db_query_fields.Snowflake <- function(con, sql, ...) {
+	fields <- dbplyr:::db_query_fields.DBIConnection(con, sql, ...)
+	tolower(fields)
+}
+
+#' @export
+collect.tbl_Snowflake <- function(x, ...) {
+	data <- dbplyr:::collect.tbl_sql(x, ...)
+	colnames(data) <- tolower(colnames(data))
+	data
+}
